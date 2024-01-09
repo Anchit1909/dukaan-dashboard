54e854e40ca8af710b4bb7285a9e64040f0b4d3d
Anchit Sinha <anchit1909@gmail.com>
2024-01-09 23:21:29 +0530

    Refactored Header and Overview Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Header } from "@/components/Header";
-import { Overview } from "@/components/Overview";
+import { Header } from "@/components/Navigation/Header";
+import { Overview } from "@/components/Overview/Overview";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
 import { TransactionTable } from "@/components/TransactionTable/TransactionTable";
 
